d0f7411128ef79d4286865f7e1080ea671cebda2
KhoiTranAnh <trananhkhoitv@gmail.com>
2026-02-04 18:44:30 +0700

    feat: back up db data and schema

--- db_schema/identity_db_data_20260204.sql
+++ db_schema/identity_db_data_20260204.sql
@@ -2,14 +2,15 @@
 -- PostgreSQL database dump
 --
 
-\restrict xfLi3ob0bpax1aZbtVT9444HIUH6vMf7njfXjLc25DkIUBULBFNwUFnu7VY4lHL
+\restrict hWEgA9aigGnlccvgXnem4KL1oS42p44xqqAidJTP3j0lkUkU4i7NYyr4nPDcORi
 
--- Dumped from database version 16.11 (Debian 16.11-1.pgdg13+1)
--- Dumped by pg_dump version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
+-- Dumped from database version 17.7 (Ubuntu 17.7-3.pgdg22.04+1)
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -28,9 +29,8 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.group_permission (
     grp_per_id bigint NOT NULL,
-    granted_at timestamp without time zone,
     valid_until timestamp without time zone,
-    created_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp without time zone,
     is_active boolean,
     group_id bigint NOT NULL,
@@ -60,10 +60,10 @@ ALTER TABLE public.group_permission ALTER COLUMN grp_per_id ADD GENERATED ALWAYS
 
 CREATE TABLE public.groups (
     group_id bigint NOT NULL,
-    group_name character(255) NOT NULL,
-    group_des character(255),
+    group_name character varying(255) NOT NULL,
+    group_des character varying(255),
     is_active boolean,
-    created_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp without time zone
 );
 
@@ -90,11 +90,11 @@ ALTER TABLE public.groups ALTER COLUMN group_id ADD GENERATED ALWAYS AS IDENTITY
 
 CREATE TABLE public.permissions (
     per_id bigint NOT NULL,
-    per_code bigint NOT NULL,
-    per_name character(255) NOT NULL,
-    per_des character(255),
+    per_code character varying(255) NOT NULL,
+    per_name character varying(255) NOT NULL,
+    per_des character varying(255),
     is_active boolean,
-    created_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp without time zone
 );
 
@@ -121,10 +121,11 @@ ALTER TABLE public.permissions ALTER COLUMN per_id ADD GENERATED ALWAYS AS IDENT
 
 CREATE TABLE public.user_group (
     user_group_id bigint NOT NULL,
-    added_at timestamp without time zone,
-    is_active timestamp without time zone,
+    added_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     user_id bigint NOT NULL,
-    group_id bigint NOT NULL
+    group_id bigint NOT NULL,
+    is_active boolean DEFAULT true NOT NULL,
+    updated_at timestamp without time zone
 );
 
 
@@ -153,7 +154,7 @@ CREATE TABLE public.users (
     user_email character varying(255) NOT NULL,
     hashed_pw character varying(255) NOT NULL,
     updated_at timestamp without time zone,
-    created_at timestamp without time zone
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -177,7 +178,15 @@ ALTER TABLE public.users ALTER COLUMN user_id ADD GENERATED ALWAYS AS IDENTITY (
 -- Data for Name: group_permission; Type: TABLE DATA; Schema: public; Owner: khoidev
 --
 
-COPY public.group_permission (grp_per_id, granted_at, valid_until, created_at, updated_at, is_active, group_id, per_id) FROM stdin;
+COPY public.group_permission (grp_per_id, valid_until, created_at, updated_at, is_active, group_id, per_id) FROM stdin;
+1	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	1
+2	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	2
+3	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	3
+4	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	4
+5	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	5
+6	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	6
+7	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	7
+8	2027-01-01 00:00:00	2026-02-04 17:28:32.003023	\N	t	1	8
 \.
 
 
@@ -186,6 +195,9 @@ COPY public.group_permission (grp_per_id, granted_at, valid_until, created_at, u
 --
 
 COPY public.groups (group_id, group_name, group_des, is_active, created_at, updated_at) FROM stdin;
+1	ADMIN	System administrators	t	2026-02-04 17:17:36.84909	\N
+2	BUYER	End users / buyers	t	2026-02-04 17:17:36.84909	\N
+3	GUEST	Limited-access guest users	t	2026-02-04 17:17:36.84909	\N
 \.
 
 
@@ -194,6 +206,14 @@ COPY public.groups (group_id, group_name, group_des, is_active, created_at, upda
 --
 
 COPY public.permissions (per_id, per_code, per_name, per_des, is_active, created_at, updated_at) FROM stdin;
+1	USER_VIEW	View users	View user list and user details	t	2026-02-01 09:58:51.623872	\N
+2	USER_CREATE	Create user	Create new user accounts	t	2026-02-01 09:58:51.623872	\N
+3	USER_UPDATE	Update user	Update user information	t	2026-02-01 09:58:51.623872	\N
+4	USER_DELETE	Delete user	Remove or deactivate users	t	2026-02-01 09:58:51.623872	\N
+5	GROUP_VIEW	View groups	View user groups	t	2026-02-01 09:58:51.623872	\N
+6	GROUP_MANAGE	Manage groups	Create, update, delete groups	t	2026-02-01 09:58:51.623872	\N
+7	PERMISSION_VIEW	View permissions	View permission list	t	2026-02-01 09:58:51.623872	\N
+8	PERMISSION_MANAGE	Manage permissions	CRUD permission and grant-revoke permission to groups	t	2026-02-01 09:58:51.623872	\N
 \.
 
 
@@ -201,7 +221,8 @@ COPY public.permissions (per_id, per_code, per_name, per_des, is_active, created
 -- Data for Name: user_group; Type: TABLE DATA; Schema: public; Owner: khoidev
 --
 
-COPY public.user_group (user_group_id, added_at, is_active, user_id, group_id) FROM stdin;
+COPY public.user_group (user_group_id, added_at, user_id, group_id, is_active, updated_at) FROM stdin;
+1	2026-02-04 17:26:36.15115	1	1	t	\N
 \.
 
 
@@ -210,6 +231,7 @@ COPY public.user_group (user_group_id, added_at, is_active, user_id, group_id) F
 --
 
 COPY public.users (user_id, user_email, hashed_pw, updated_at, created_at) FROM stdin;
+1	admin@gmail.com	$2a$10$TS/OQ90y76c7uc4z3va6N.5e6d0i.HWu6gescx8b9yGbo3Fnd5gdu	\N	2026-02-04 17:07:35.323448
 \.
 
 
@@ -217,35 +239,35 @@ COPY public.users (user_id, user_email, hashed_pw, updated_at, created_at) FROM 
 -- Name: group_permission_grp_per_id_seq; Type: SEQUENCE SET; Schema: public; Owner: khoidev
 --
 
-SELECT pg_catalog.setval('public.group_permission_grp_per_id_seq', 1, false);
+SELECT pg_catalog.setval('public.group_permission_grp_per_id_seq', 8, true);
 
 
 --
 -- Name: groups_group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: khoidev
 --
 
-SELECT pg_catalog.setval('public.groups_group_id_seq', 1, false);
+SELECT pg_catalog.setval('public.groups_group_id_seq', 4, true);
 
 
 --
 -- Name: permissions_per_id_seq; Type: SEQUENCE SET; Schema: public; Owner: khoidev
 --
 
-SELECT pg_catalog.setval('public.permissions_per_id_seq', 1, false);
+SELECT pg_catalog.setval('public.permissions_per_id_seq', 8, false);
 
 
 --
 -- Name: user_group_user_group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: khoidev
 --
 
-SELECT pg_catalog.setval('public.user_group_user_group_id_seq', 1, false);
+SELECT pg_catalog.setval('public.user_group_user_group_id_seq', 1, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: khoidev
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 3, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 1, true);
 
 
 --
@@ -305,6 +327,14 @@ ALTER TABLE ONLY public.user_group
 
 
 --
+-- Name: user_group user_group_user_id_group_id_key; Type: CONSTRAINT; Schema: public; Owner: khoidev
+--
+
+ALTER TABLE ONLY public.user_group
+    ADD CONSTRAINT user_group_user_id_group_id_key UNIQUE (user_id, group_id);
+
+
+--
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: khoidev
 --
 
@@ -356,5 +386,5 @@ ALTER TABLE ONLY public.user_group
 -- PostgreSQL database dump complete
 --
 
-\unrestrict xfLi3ob0bpax1aZbtVT9444HIUH6vMf7njfXjLc25DkIUBULBFNwUFnu7VY4lHL
+\unrestrict hWEgA9aigGnlccvgXnem4KL1oS42p44xqqAidJTP3j0lkUkU4i7NYyr4nPDcORi
 
